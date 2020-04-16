614db15c7cfc2ac856219f83663a2dd9de45b256
Zach Sharpe <zach@m1460.local>
2020-04-16 14:55:07 -0700

    cleaned up game logic with functions

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,19 +3,23 @@ console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-console.log("Card flipped - " + cardOne);
-
-cardsInPlay.push(cardTwo);
-console.log("Card flipped - " + cardTwo);
-
-if(cardsInPlay.length === 2) {
-    if(cardsInPlay[0] === cardsInPlay[1]) {
-        alert("you found a match!");
-    } else {
-        alert("sorry, not a match");
+function checkForMatch(){
+    if(cardsInPlay.length === 2) {
+        if(cardsInPlay[0] === cardsInPlay[1]) {
+            alert("you found a match!");
+        } else {
+            alert("sorry, not a match");
+        }
     }
-}
+};
+
+function flipCard(cardID){
+    
+    console.log("User flipped " + cards[cardID]);
+    cardsInPlay.push(cards[cardID]);
+
+    checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
