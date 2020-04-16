004be23231ed829e5b0ff9307f28e9d05ae23cda
Zach Sharpe <zach@m1460.local>
2020-04-16 14:13:11 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,21 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
-console.log("user flipped " + cardOne)
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("Card flipped - " + cardOne);
+
+cardsInPlay.push(cardTwo);
+console.log("Card flipped - " + cardTwo);
+
+if(cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("you found a match!");
+    } else {
+        alert("sorry, not a match");
+    }
+}
